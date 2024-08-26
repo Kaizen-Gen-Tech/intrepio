c0fb8da10e9e37a0afe021c8be6f579a37606b41
Justin Henry <jdhenry08@gmail.com>
2024-08-26 16:54:10 +0000

    Add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { type Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
+import { cn } from "~/lib/utils";
+
 export const metadata: Metadata = {
   title: "INT.REP.IO",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
@@ -13,7 +15,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={cn(GeistSans.variable, GeistMono.variable)}>
       <body>{children}</body>
     </html>
   );
