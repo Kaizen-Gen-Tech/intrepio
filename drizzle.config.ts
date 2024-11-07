358343fad52d5086e35b7475c40b74240e4b5f0e
Justin Henry <jdhenry08@gmail.com>
2024-11-07 20:14:25 +0000

    Update env config and create initial db schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.POSTGRES_URL,
   },
 } satisfies Config;
